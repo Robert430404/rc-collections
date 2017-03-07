437375cec761c18ab26eb8630c824a96840fdcea
Robert Cox <rcox@dontbebroke.com>
2017-03-07 15:49:04 -0800

    Updated the doc blocks

--- src/RcCollections/Collection.php
+++ src/RcCollections/Collection.php
@@ -25,33 +25,11 @@ class Collection
     }
 
     /**
-     * Performs a map on the data
+     * Performs the key case change function on the data and, then returns the collection
+     * for further manipulation of the data by the user.
      *
-     * @param callable $handle
-     * @return Collection
-     */
-    public function map(Callable $handle): Collection
-    {
-        $this->data = array_map($handle, $this->data);
-
-        return $this;
-    }
-
-    /**
-     * Performs a filter on the data
-     *
-     * @param callable $handle
-     * @return Collection
-     */
-    public function filter(Callable $handle): Collection
-    {
-        $this->data = array_filter($this->data, $handle, ARRAY_FILTER_USE_BOTH);
-
-        return $this;
-    }
-
-    /**
-     * Performs the key case change function on the data
+     * The array change key case function upper or lower cases all of the characters in your
+     * array kets.
      *
      * @param int $case
      * @return Collection
@@ -64,7 +42,60 @@ class Collection
     }
 
     /**
-     * Returns the result of the previous operations
+     * Performs a chunk on the data, and then returns the collection for further manipulation
+     * of the data by the user.
+     *
+     * The array chunk function works by splitting the array values into two equal, or as close
+     * as possible, size arrays.
+     *
+     * @param int $size
+     * @param bool $preserveKeys
+     * @return Collection
+     */
+    public function chunk(int $size, bool $preserveKeys = false): Collection
+    {
+        $this->data = array_chunk($this->data, $size, $preserveKeys);
+
+        return $this;
+    }
+
+    /**
+     * Performs a map on the data, and then returns the collection for further manipulation
+     * of the data by the user.
+     *
+     * The array map applies all of the mutations you have in your handler to the value of the
+     * array you are currently manipulating.
+     *
+     * @param callable $handle
+     * @return Collection
+     */
+    public function map(Callable $handle): Collection
+    {
+        $this->data = array_map($handle, $this->data);
+
+        return $this;
+    }
+
+    /**
+     * Performs a filter on the data, and then returns the collection for further manipulation
+     * of the data by the user.
+     *
+     * The array filter function works by filtering the array values based on the parameters you
+     * have set in your handler function
+     *
+     * @param callable $handle
+     * @param int $mode
+     * @return Collection
+     */
+    public function filter(Callable $handle, int $mode = ARRAY_FILTER_USE_BOTH): Collection
+    {
+        $this->data = array_filter($this->data, $handle, $mode);
+
+        return $this;
+    }
+
+    /**
+     * Returns the result of the operations that were done to the collection.
      *
      * @return array
      */
